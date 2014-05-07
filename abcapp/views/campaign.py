9afdb77a3e2433b84d04e45fd16c97e9077b8d6d
ZombieToof <z0mb13t00f@gmail.com>
2014-05-07 13:04:38 +0200

    Add join campaign view

--- abcapp/views/campaign.py
+++ abcapp/views/campaign.py
@@ -1,7 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from abcapp.models import Campaign
+from abcapp.models import CampaignParticipation
 
 
 class CampaignList(ListView):
@@ -16,3 +18,39 @@ class CampaignDetailView(DetailView):
     model = Campaign
     context_object_name = 'campaign'
     template_name = 'campaigns/details.html'
+
+
+class CampaignJoinView(DetailView):
+
+    model = Campaign
+    context_object_name = 'campaign'
+    template_name = 'campaigns/join.html'
+
+    def get_context_data(self, **context):
+        context = super(CampaignJoinView, self).get_context_data(**context)
+        campaign = context['campaign']
+        user = getattr(self.request, 'user', None)
+        context['user'] = user
+
+        try:
+            player = user.player
+        except (AttributeError, ObjectDoesNotExist):
+            context['no_player'] = True
+            return context
+
+        participation = CampaignParticipation.objects.filter(
+            player=player, campaign=campaign).first()
+
+        if participation is not None:
+            context['already_joined'] = True
+            context['participation'] = participation
+            return context
+
+        title = u'User %s participates in Campaign %s' % (
+            player.phpbb_user.username, campaign.title)
+        slug = '%s-%s' % (player.phpbb_user.username, campaign.title)
+        participation = CampaignParticipation.objects.create(
+            player=player, campaign=campaign, title=title, slug=slug)
+        context['participation'] = participation
+        context['joined'] = True
+        return context
