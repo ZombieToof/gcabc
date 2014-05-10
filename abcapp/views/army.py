6edb9d64407703d0004c94335241269049ea9e79
ZombieToof <z0mb13t00f@gmail.com>
2014-05-10 17:37:46 +0200

    /campaign/<pk>/army List of armies in a campaign
    Very basic view

--- abcapp/views/army.py
+++ abcapp/views/army.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from abcapp.models import Army
+from abcapp.models import Campaign
 
 
 class ArmyListView(ListView):
@@ -12,3 +14,16 @@ class ArmyListView(ListView):
 
     def get_queryset(self):
         return Army.objects.order_by('campaign__start')
+
+
+class CampaignArmyListView(ArmyListView):
+
+    template_name = 'army/campaign_index.html'
+
+    def get_context_data(self, **context):
+        q = super(CampaignArmyListView, self).get_queryset()
+        campaign = get_object_or_404(Campaign, id=self.kwargs['pk'])
+        armies = q.filter(campaign=campaign).all()
+        context['armies'] = armies
+        context['campaign'] = campaign
+        return context
