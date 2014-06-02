3311c9ecadae03f1c4f20bf60c486902672d930a
ZombieToof <z0mb13t00f@gmail.com>
2014-06-02 20:59:14 +0200

    [ng] Add an angular-generator/yeoman project
    Also check in the bower_components.

---
+++ abcapp/static/ng/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('abcclientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
