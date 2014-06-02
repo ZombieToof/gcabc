3c1d1d72f894c1c91c9eeda4addd3915ab13e67b
ZombieToof <z0mb13t00f@gmail.com>
2014-06-02 21:00:09 +0200

    Add a staticfilesfinder that ignores node_modules - missing file

--- abcapp/static.py
+++ abcapp/static.py
@@ -2,6 +2,15 @@ from django.contrib.staticfiles.finders import AppDirectoriesFinder
 
 
 class NpmFreeAppDirectoriesFinder(AppDirectoriesFinder):
+    '''
+    A finder for app directories that automatically adds
+    node_modules to the list of files not to list.
+
+    node_modules can be huge (>11000 files), will never be served and
+    slows down finder.list() to a crawl. Usually this is not bad, but
+    django-debug-toolbar calls list() for every page view, so it can
+    easily add 10 Seconds to each page reload.
+    '''
 
     def list(self, ignore_pattern):
         if ignore_pattern is None:
